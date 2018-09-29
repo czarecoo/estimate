ae6dc56a8ce8646d2f58bd93c86766da1c3cb10d
Cezary Witkowski <czarecoo@gmail.com>
2018-09-29 16:57:24 +0200

    Finish story should be shown instead preview not whole creator view

--- src/CreatorView.js
+++ src/CreatorView.js
@@ -29,10 +29,20 @@ class CreatorView extends React.Component {
 	render() {
 		if (this.state.isFinishingStory) {
 			return (
-				<div className="CreatorFinishStoryView">
-					Finish Story <button onClick={this.handleFinishStory.bind(this)}>X</button><br></br>
-					<VoteTable story={this.state.previewStory} />
-					<CreatorViewFinalVote userStory={this.state.previewStory} onFinishStory={this.handleFinishStory.bind(this)} closePreview={this.handlePreviewChange.bind(this)} />
+				<div className="CreatorView row">
+					<div className="col-xs-12 col-md-4 col-lg-4 col-xl-4">
+						List of future stories:<br></br>
+						<CreatorViewStoriesList storyList={this.props.data.futureStories} onSelectingStory={this.handlePreviewChange.bind(this)} canAdd={true} />
+						Current Story:<br></br>
+						<CreatorViewStoriesList storyList={this.props.data.currentStory} onSelectingStory={this.handlePreviewChange.bind(this)} canAdd={false} />
+						List of finished stories:<br></br>
+						<CreatorViewStoriesList storyList={this.props.data.pastStories} onSelectingStory={this.handlePreviewChange.bind(this)} canAdd={false} />
+					</div>
+					<div className="col-xs-12 col-md-8 col-lg-8 col-xl-8">
+						Finish Story <button onClick={this.handleFinishStory.bind(this)}>X</button><br></br>
+						<VoteTable story={this.state.previewStory} />
+						<CreatorViewFinalVote userStory={this.state.previewStory} onFinishStory={this.handleFinishStory.bind(this)} closePreview={this.handlePreviewChange.bind(this)} />
+					</div>
 				</div>
 			);
 		} else {
