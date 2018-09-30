053198f68eb401cd05de61833d95116600200d12
Cezary Witkowski <czarecoo@gmail.com>
2018-09-30 14:48:17 +0200

    Moved testing button

--- src/VoteViewLoginInfo.js
+++ src/VoteViewLoginInfo.js
@@ -9,8 +9,7 @@ class VoteViewLoginInfo extends React.Component {
 	render() {
 		return (
 			<div className="VoteViewLoginInfo">
-				<button onClick={() => window.location.reload()}>Close without emits (for testing only) </button>
-				SessionID: <b>{this.props.data.sessionId}</b>{this.props.data.isSuperUser ? <button onClick={this.refresh.bind(this)}>Close session</button> : null}<br></br>
+				SessionID: <b>{this.props.data.sessionId}</b>{this.props.data.isSuperUser ? <button onClick={this.refresh.bind(this)}>Close session</button> : null} <button onClick={() => window.location.reload()}>Close without emits (for testing only) </button><br></br>
 				My name: <b>{this.props.data.login}</b><br></br>
 				My name: <b>{this.props.data.userId}</b><br></br>
 				My SocketID: <b>{SocketManager.getSocketId()}</b><br></br><br></br>
